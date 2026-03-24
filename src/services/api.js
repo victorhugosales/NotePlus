bf5bd9652747cd70e001dd1728fbdd13f9156f24
Lendariofull <victorpazsales@gmail.com>
2026-03-24 15:07:43 -0300

    new test ambient

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://noteplusbackend.onrender.com', 
+  baseURL: import.meta.env.VITE_API_URL || 'https://noteplusbackend.onrender.com',
 });
 
 // Isso adiciona o token em toda chamada da API automaticamente
