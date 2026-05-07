10ddf09d91528cfd95187fd72108853c4d2b849b
victorhugosales <vh9876543211@gmail.com>
2026-05-07 19:23:49 -0300

    corrigindo url do service

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //Para o vercel:
-  baseURL: import.meta.env.VITE_API_URL || 
+  baseURL: import.meta.env.VITE_API_URL || 'https://homologacaonoteplusbackend.onrender.com'
 
   //Produção:
   //baseURL: 'https://noteplusbackend.onrender.com'
