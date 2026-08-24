b28c205115ae49fe8659bcdac4484cf8cbf4ad43
victorhugosales <vh9876543211@gmail.com>
2026-08-24 08:43:23 -0300

    teste deploy homologacao

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //Para o vercel:
-  //baseURL: import.meta.env.VITE_API_URL || 'https://homologacaonoteplusbackend.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL || 'https://homologacaonoteplusbackend.onrender.com'
 
   //Produção:
   //baseURL: 'https://noteplusbackend.onrender.com'
@@ -11,7 +11,7 @@ const api = axios.create({
   //baseURL: 'https://homologacaonoteplusbackend.onrender.com'
 
   //Para testes locais:
-  baseURL: 'http://localhost:3333'
+  //baseURL: 'http://localhost:3333'
 });
 
 // Isso adiciona o token em toda chamada da API automaticamente
